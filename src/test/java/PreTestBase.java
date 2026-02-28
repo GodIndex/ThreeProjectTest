75c3e4dc7785591075edab3b4f612b1167c5517e
GodIndex <mountyxio@mail.ru>
2026-02-28 12:26:24 +0400

    Create step
    Signed-off-by: GodIndex <mountyxio@mail.ru>

---
+++ src/test/java/PreTestBase.java
@@ -0,0 +1,84 @@
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.pageLoadTimeout;
+import static com.codeborne.selenide.Selenide.sleep;
+
+
+
+class PreTestBase {
+    //  пред-шаги
+    @BeforeAll
+    static void beforeAll() {
+        baseUrl = "https://topgo.group/";
+        Configuration.browser = "Chrome";
+        Configuration.browserSize = "1920x1080";
+
+    }
+    public void ValidatorOpenURL() {
+        switch (baseUrl) {
+            case ("https://topgo.group/"):
+                System.out.println("URL открылся");
+                Selenide.open(baseUrl);
+                break;
+        }
+    }
+    ///  коллекция локаторов button media соцсетей и использование на всех них hover эффекта
+
+    private final  ElementsCollection ItemsHeaderMediaLocatorThree = Selenide.$$("a.header__media");
+
+    void HoverEffectMediaButton() {
+
+        ItemsHeaderMediaLocatorThree.get(0).hover();
+        ItemsHeaderMediaLocatorThree.get(1).hover();
+        ItemsHeaderMediaLocatorThree.get(2).hover();
+
+    }
+    /// локатор и клик кнопки вакансии из списка 3 кнопок
+
+    private final SelenideElement VacantionLocatorButtons = Selenide.$x("//a[@href =\"https://spb.hh.ru/employer/11013820?hhtmFrom=vacancy\" and @class =\"header__link\"]");
+
+    void VacantionsButtonClick() {
+        VacantionLocatorButtons.click();
+
+    }
+    /// локатор и проверка первого слова в описании после нажатия вакансии
+
+    private final  SelenideElement CompanyInfoClick = Selenide.$x("employer-page-tabs-desktop-go-DESCRIPTION employer-page-tabs-desktop-active");
+    void InfoCompanyNextPage() {
+        CompanyInfoClick.click();
+    }
+
+    /// локатор и проверка ссылки обратно на главную страницу сайта
+
+    private final SelenideElement ShouldBeLinkNameCompanyLocator = Selenide.$x("//div[text() = 'https://topgo.group/']");
+
+    void ValidatorLinkReverseTransitionHomePage() {
+        ShouldBeLinkNameCompanyLocator.shouldBe((Condition.text("https://topgo.group/")));
+    }
+
+    /// локатор и проверка действия hover при наведении на рейтинг, клик на рейтинг для открытия выборки
+
+    private final SelenideElement HoverForRatimgJob = Selenide.$("a.link-to-settings--eFyJRA6gx4F1Wwkd");
+    void OpenHoverEffectRating() { HoverForRatimgJob.hover(); }
+
+    private final  SelenideElement FormForVariationsJob = Selenide.$("a.link-to-settings--eFyJRA6gx4F1Wwkd");
+    void OpenNewFormVariationsJobs() { FormForVariationsJob.click(); }
+
+
+
+
+    ///  после шаги
+    @AfterAll
+    static void afterAll() {
+        sleep(1_000);
+    }
+
+
+
+
+
+
+
+}
