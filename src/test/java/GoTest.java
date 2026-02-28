d9ee54b3a4f1715015f573ce49e4da8cebe8ff83
GodIndex <mountyxio@mail.ru>
2026-02-28 17:45:56 +0400

    3 test steps

--- src/test/java/GoTest.java
+++ src/test/java/GoTest.java
@@ -1,8 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 public class GoTest extends PreTestBase {
+
     @Test
     void FirstUnTest() {
+
         ValidatorOpenURL();
+        HoverEffectMediaButton();
+        VacantionsButtonClick();
+
     }
 }
