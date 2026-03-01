fc2b3d2cd4706ea31104307a3fb783d0382dba84
GodIndex <mountyxio@mail.ru>
2026-03-01 16:34:43 +0400

    Attach step

--- src/test/java/GoTest.java
+++ src/test/java/GoTest.java
@@ -1,13 +1,31 @@
-import org.junit.jupiter.api.Test;
+import helpers.Attach;
+import org.junit.jupiter.api.*;
 
 public class GoTest extends PreTestBase {
 
+
+
+    @AfterEach
+    void addAttachment() {
+        Attach.screenshotAs("Last Screen");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+    }
+
+
+    @Tag("TogGo Collection")
+    @DisplayName("TopGo Tests")
+
     @Test
+
     void FirstUnTest() {
 
         BrowserCongiureValidatorOpenPage();
         HoverEffectMediaButton();
         VacantionsButtonClick();
+        InfoCompanyNextPage();
+        ValidatorLinkReverseTransitionHomePage();
+
 
     }
 }
